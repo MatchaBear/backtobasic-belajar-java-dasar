9094d374dc972e477edcf08f68250cea68d01b6e
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-26 19:56:48 +0800

    keep learning

---
+++ src/DataNumberType.java
@@ -0,0 +1,23 @@
+package src;
+
+public class DataNumberType {
+
+    public static void main(String[] args) {
+
+        byte iniByte = 100;
+        short iniShort = 1000;
+        long iniLong = 1_000_000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 25;
+        int hexInt = 0xA123B;
+        int binInt = 0b101001;
+
+        long balance = 1_000_000_000;
+        int sum = 2_000;
+
+
+    }
+}
---
+++ src/NumberConversion.java
@@ -0,0 +1,15 @@
+package src;
+
+public class NumberConversion {
+
+    public static void main(String[] args) {
+
+        byte iniByte = 10;
+        short iniShort = iniByte; /* conversion from byte to short */
+        int iniInt = iniShort;
+
+        int iniInt2 = 1000;
+        byte iniByte2 = (byte) iniInt2;
+
+    }
+}
